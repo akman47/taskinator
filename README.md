7619d22a774f6718735c7412deb072153e4db89b
Angela Man <angela.k.man@gmail.com>
2021-04-03 16:41:41 -0500

    save tasks to array

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# taskinator
+# Taskinator
+
+## Purpose
+A website that allows users to create tasks and track their status
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+https://akman47.github.io/taskinator
+![snapshot of taskinator website with tasks organized in status columns](./assets/images/taskinator.png)
+
+## Contribution
+Made by Angela Man
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
-var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
+var pageContentEl = document.querySelector("#page-content");
+var tasks = [];
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -27,8 +28,9 @@ var taskFormHandler = function(event) {
         // package up data as an object
         var taskDataObj = {
             name: taskNameInput,
-            type: taskTypeInput
-        };
+            type: taskTypeInput,
+            status: "to do"
+        }
 
         // send it as an argument to createTaskEl
         createTaskEl(taskDataObj);
@@ -55,6 +57,9 @@ var createTaskEl = function(taskDataObj) {
     
     listItemEl.appendChild(taskInfoEl);
 
+    taskDataObj.id = taskIdCounter;
+    tasks.push(taskDataObj);
+
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
 
@@ -63,6 +68,9 @@ var createTaskEl = function(taskDataObj) {
 
     // increase task counter for next unique id
     taskIdCounter++;
+
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
 }
 
 // create buttons that correspond to current task id
@@ -107,8 +115,6 @@ var createTaskActions = function(taskId) {
     return actionContainerEl;
 };
 
-formEl.addEventListener("submit", taskFormHandler);
-
 // assign delete and edit buttons to data-task-ids
 var taskButtonHandler = function(event) {
 
@@ -125,12 +131,24 @@ var taskButtonHandler = function(event) {
     }
 };
 
-pageContentEl.addEventListener("click", taskButtonHandler);
-
 // delete tasks from tasks-to-do
 var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
+
+    // create a new array to hold updated list of tasks
+    var updatedTaskArr = [];
+
+    // loop through current tasks
+    for (var i = 0; i < tasks.length; i++) {
+        // if tasks[i].id doesn't match the value of taskId, keep that task and push it into the new array
+        if (tasks[i].id != parseInt(taskId)) {
+            updatedTaskArr.push(tasks[i]);
+        }
+    }
+
+    // reassign tasks array to be the same as updatedTaskArr
+    tasks = updatedTaskArr;
 };
 
 // update form entry with task name and type for editing
@@ -158,6 +176,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
     taskSelected.querySelector("h3.task-name").textContent = taskName;
     taskSelected.querySelector("span.task-type").textContent = taskType;
 
+    // loop through tasks array and task object with new content
+    for (var i=0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].name = taskName;
+            tasks[i].type = taskType;
+        }
+    };
+
     alert("Task Updated!");
 
     formEl.removeAttribute("data-task-id");
@@ -184,7 +210,22 @@ var taskStatusChangeHandler = function (event) {
     else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
     }
+
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].status = statusValue;
+        }
+    }
+
+    console.log(tasks);
 };
 
+// create a new task
+formEl.addEventListener("submit", taskFormHandler);
+
+// for edit and delete buttons
+pageContentEl.addEventListener("click", taskButtonHandler);
+
+// for changing the task status
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 
